7f6e6960de2ce1084826953a5670aa7ee8532575
Artemish <mitch.cro@gmail.com>
2022-05-26 18:43:49 -0500

    Include FLAC is music file regex

--- fresher.py
+++ fresher.py
@@ -12,7 +12,7 @@ FRESHER_DIR=os.path.expanduser("~/.cache/fresher")
 FRESHER_CACHE=f"{FRESHER_DIR}/cache.json"
 
 MUSIC_DIRECTORY = os.path.expanduser("~/Music")
-MUSIC_REGEX = "(.+)\.(mp3|ogg|opus|m4a|mp4)$"
+MUSIC_REGEX = "(.+)\.(mp3|ogg|opus|m4a|mp4|flac)$"
 
 BASE_SCORE=100
 
